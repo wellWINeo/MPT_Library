c8ecd5098045e02d626f645574ff96b9e85a1803
wellWINeo <usa200313@gmail.com>
2022-03-03 18:49:02 +0300

    Authors page

--- ViewModels/AuthorsViewModel.cs
+++ ViewModels/AuthorsViewModel.cs
@@ -1,16 +1,58 @@
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Reactive;
+using System.Threading.Tasks;
+using Library.Models;
 using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
 
 namespace Library.ViewModels;
 
 public class AuthorsViewModel : ViewModelBase
 {
+    [Reactive] public int SelectedIdx { get; set; }
+    [Reactive] public string Name { get; set; }
     
+    public  ObservableCollection<Author> Authors { get; set; }
+ 
     public ReactiveCommand<Unit, IRoutableViewModel?> GoToBooks { get; }
+    
+    public ReactiveCommand<Unit, Unit> AddCommand { get; }
+    public ReactiveCommand<Unit, Unit> UpdateCommand { get; }
+    public ReactiveCommand<Unit, Unit> DeleteCommand { get; }
 
     public AuthorsViewModel()
     {
+        Authors = new(db.Authors);
         GoToBooks = HostScreen.Router.NavigateBack;
+
+        AddCommand = ReactiveCommand.CreateFromTask(add);
+        UpdateCommand = ReactiveCommand.CreateFromTask(update);
+        DeleteCommand = ReactiveCommand.CreateFromTask(delete);
+    }
+
+    private async Task add()
+    {
+        var author = new Author { Name = Name };
+        
+        Authors.Add(author);
+        await db.Authors.AddAsync(author);
+        await db.SaveChangesAsync();
+    }
+
+    private async Task update()
+    {
+        Authors[SelectedIdx].Name = Name;
+        db.Authors.Update(Authors[SelectedIdx]);
+        await db.SaveChangesAsync();
+    }
+
+    private async Task delete()
+    {
+        db.Authors.Remove(Authors[SelectedIdx]);
+        Authors.RemoveAt(SelectedIdx);
+        await db.SaveChangesAsync();
     }
     
 }
--- Views/AuthorsView.axaml.cs
+++ Views/AuthorsView.axaml.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Mixins;
 using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
+using Library.Models;
 using Library.ViewModels;
 using ReactiveUI;
 
@@ -18,6 +21,27 @@ public partial class AuthorsView : ReactiveUserControl<AuthorsViewModel>
         {
             this.BindCommand(ViewModel, vm => vm.GoToBooks, view => view.GoToBooksButton)
                 .DisposeWith(d);
+
+            this.BindCommand(ViewModel, vm => vm.AddCommand, view => view.AddButton)
+                .DisposeWith(d);
+            this.BindCommand(ViewModel, vm => vm.UpdateCommand, view => view.ChangeButton)
+                .DisposeWith(d);
+            this.BindCommand(ViewModel, vm => vm.DeleteCommand, view => view.RemoveButton)
+                .DisposeWith(d);
+
+            this.Bind(ViewModel, vm => vm.SelectedIdx, view => view.Grid.SelectedIndex)
+                .DisposeWith(d);
+            this.Bind(ViewModel, vm => vm.Name, view => view.Name.Text)
+                .DisposeWith(d);
+            this.Bind(ViewModel, vm => vm.Authors, view => view.Grid.Items,
+                    value => value as IEnumerable<Author>,
+                    value => value as ObservableCollection<Author>)
+                .DisposeWith(d);
         });
+    }
+
+    private void Grid_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        Name.Text = (this.Grid.SelectedItem as Author).Name ?? "";
     }
 }
