bb51b4edee94fc7e7778c7b459753513f9ba6515
wellWINeo <usa200313@gmail.com>
2022-03-03 18:52:09 +0300

    Remove test views & vms

--- ViewModels/ThirdViewModel.cs
+++
@@ -1,9 +0,0 @@
-using JetBrains.Annotations;
-using ReactiveUI;
-
-namespace Library.ViewModels;
-
-public class ThirdViewModel : ViewModelBase
-{
-    public string MessageOne => "Hi, from third";
-}
